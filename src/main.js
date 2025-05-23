f9de0ea0b6c5a9f22fe3e7e8fb45d3566230a328
melkiurak <melkiurak3209@gmail.com>
2025-05-23 13:15:43 +0300

    Add numbers

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let numbers = document.querySelectorAll('.buttons__numbers');
+let operators = document.querySelectorAll('.buttons__operators');
+const output = document.querySelector('.display__output') 
+const handelGetNumbers = (e) => {
+    if (e.target.textContent === '.' && output.textContent.includes('.')) return;
+    output.textContent = (output.textContent === '0' && e.target.textContent !== '.') ? e.target.textContent : output.textContent + e.target.textContent
+}
+const handelGetOperators = (e) => {
+    console.log('Я оператор', e.target.textContent);
+}
+const handelResult = (e) => {
+    console.log()
+}
+numbers.forEach(button => {
+    button.addEventListener("click", handelGetNumbers);
+});
+operators.forEach(button => {
+    button.addEventListener("click", handelGetOperators);
+});
+
