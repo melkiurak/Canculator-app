6e15368da11ffe2e7ba2b0c0ab3c684861132c10
melkiurak <melkiurak3209@gmail.com>
2025-05-24 12:18:36 +0300

    Fix

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-let numbers = document.querySelectorAll('.buttons__numbers');
-let operators = document.querySelectorAll('.buttons__operators');
+const allCalculatorButtons = document.querySelectorAll('.buttons__numbers, .buttons__operators, .buttons__special');
 const output = document.querySelector('.display__output') 
-const handelGetNumbers = (e) => {
-    if (e.target.textContent === '.' && output.textContent.includes('.')) return;
-    output.textContent = (output.textContent === '0' && e.target.textContent !== '.') ? e.target.textContent : output.textContent + e.target.textContent
-}
-const handelGetOperators = (e) => {
-    console.log('Я оператор', e.target.textContent);
-}
-numbers.forEach(button => {
-    button.addEventListener("click", handelGetNumbers);
-});
-operators.forEach(button => {
-    button.addEventListener("click", handelGetOperators);
-});
 
+let firstOperator = null;
+let operator = ['+', '-', '*', '/', '%'];
+
+const handleTest = (e) => {
+    const numberValue = firstOperator(e.target.textContent);
+    if(numberValue === '.' && output.textContent.includes('.')) return;
+
+    console.log(numberValue + numberValue, output.textContent)
+    output.textContent = (output.textContent === '0' && numberValue !== '.') ? numberValue : output.textContent + numberValue
+}
+allCalculatorButtons.forEach(button => {
+    button.addEventListener("click", handleTest);
+});
