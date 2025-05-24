38a844449ee205d1cd25615d20333959212181a4
melkiurak <melkiurak3209@gmail.com>
2025-05-24 12:52:07 +0300

    Fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 const allCalculatorButtons = document.querySelectorAll('.buttons__numbers, .buttons__operators, .buttons__special');
 const output = document.querySelector('.display__output') 
 
-let firstOperator = null;
-let operator = ['+', '-', '*', '/', '%'];
-
+let firstNumber = '';
+let secondNumber = '';    
+let operators = ['+', '-', '*', '/', '%']
 const handleTest = (e) => {
-    const numberValue = firstOperator(e.target.textContent);
-    if(numberValue === '.' && output.textContent.includes('.')) return;
-
-    console.log(numberValue + numberValue, output.textContent)
-    output.textContent = (output.textContent === '0' && numberValue !== '.') ? numberValue : output.textContent + numberValue
+    const value = e.target.textContent;
+    if(value === '.' && output.textContent.includes('.')) return;
+    if(!isNaN(value) || value === '.' && output.textContent.includes('')){
+        firstNumber += value
+        output.textContent = firstNumber
+        console.log(output.textContent)
+    }else if(operators.includes(value)) {
+        if(firstNumber === '') return
+        secondNumber = firstNumber
+        operators = value
+        firstNumber = ''
+    }
 }
 allCalculatorButtons.forEach(button => {
     button.addEventListener("click", handleTest);
