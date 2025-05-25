47b7e93a7b42abb83859e4e4b4a710706df04fe5
melkiurak <melkiurak3209@gmail.com>
2025-05-25 12:07:00 +0300

    special btn

--- src/test.js
+++ src/test.js
@@ -1,9 +1,11 @@
 const numberButtons =  document.querySelectorAll('.buttons__numbers');
 const operatorsButtons = document.querySelectorAll('.buttons__operators');
+const specialButtons = document.querySelectorAll('.buttons__special');
 const output = document.querySelector('.display__output');
 
 let expression  = '';
 let operators = ['+', '-', '*', '/', '%']
+
 const handleNumbers = (e) => {
     expression += e.target.textContent;
     output.textContent = expression;
@@ -17,10 +19,24 @@ const handleOperators = (e) => {
     }
     output.textContent = expression
 }
-
+const handleSpecial = (e) => {
+    if(e.target.textContent === 'd'){
+        if(output.textContent === '0') return;
+        expression = output.textContent.slice(0, -1)
+        output.textContent = expression === '' ?  '0' : expression;
+    } else if (e.target.textContent === 'Ac') {
+        if(output.textContent === '0') return;
+        expression = output.textContent = ''
+        output.textContent = expression === '' ? '0' : expression;
+        console.log('AC')
+    }
+}
 numberButtons.forEach(button => {
     button.addEventListener("click", handleNumbers);
 });
 operatorsButtons.forEach(button => {
     button.addEventListener("click", handleOperators);
+});
+specialButtons.forEach(button => {
+    button.addEventListener("click", handleSpecial);
 });
